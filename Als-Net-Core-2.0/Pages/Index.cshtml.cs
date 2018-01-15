68645f2469f83d12db6ce7ffa4dc05d30f85848b
Troonk <tvangest@gmail.com>
2018-01-15 22:40:11 +0100

    Startowe wersja aplikacji

---
+++ Als-Net-Core-2.0/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Als_Net_Core_2.Data;
+using Als_Net_Core_2.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Als_Net_Core_2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private MySqlContext _db;
+
+        public IList<League> Leagues { get; set; }
+
+        public IndexModel(MySqlContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Leagues = await _db.Leagues.ToListAsync();
+        }
+    }
+}
---
+++ Als-Net-Core-2.0/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Als_Net_Core_2.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Als_Net_Core_2._0
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MySqlContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
